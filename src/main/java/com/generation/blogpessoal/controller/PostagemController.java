1bbadcc496c5608c6a8d9f47d7b66bb85b8df90e
Mina Yura <minammonteiro2@gmail.com>
2025-03-05 12:07:25 -0300

    GET/POST/PUT/DEL

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,52 +1,103 @@
 package com.generation.blogpessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
 
+import jakarta.validation.Valid;
+
 //metodos
 
-
-
 @RestController // ela vai dizer para o string que essa controller é de uma api
-@CrossOrigin(allowedHeaders = "*", origins = "*") //e de onde vou aceitar conexoes, do keito que está eu aceito de qualquer lugar
-@RequestMapping ("/postagens")//endereco da controller 
+@CrossOrigin(allowedHeaders = "*", origins = "*") // e de onde vou aceitar conexoes, do keito que está eu aceito de
+													// qualquer lugar
+@RequestMapping("/postagens") // endereco da controller
 public class PostagemController {
-	
-	@Autowired //ela cria a instancia, roda oque precisa e fecha sozinha, quando eu precisar.
+
+	@Autowired // ela cria a instancia, roda oque precisa e fecha sozinha, quando eu precisar.
 	//
 	private PostagemRepository postagemRepository;
-	
-	
+
+//SELECT
 	@GetMapping
-	public ResponseEntity<List <Postagem>> getAll(){
-		       //ResponseEntity responsavel por criar o json //vai usar a interface // e vai pegar todas
+	public ResponseEntity<List<Postagem>> getAll() {
+		// ResponseEntity responsavel por criar o json //vai usar a interface // e vai
+		// pegar todas
 		return ResponseEntity.ok(postagemRepository.findAll());
-		//quando eu fizer uma requisiçaõ de get para o /postagem, eu quero retornar do meu postagemRepository o find all
+		// quando eu fizer uma requisiçaõ de get para o /postagem, eu quero retornar do
+		// meu postagemRepository o find all
 	}
 
-	
-	@GetMapping ("/{id}")
-	                                         // identifica uma variavel nesse cado é do tipo Long que se chama id
-	public ResponseEntity <Postagem> getById(@PathVariable Long id){
-		//ele vai no banco de dados e procura a postagem pela id, se encontrar ele armazena na resposta e retorna 
-		//se não encontrar ele retorna uma mensagem de erra (404) e fecha a ação.
-		return postagemRepository.findById(id)
-				.map(resposta -> ResponseEntity.ok(resposta) )
+	@GetMapping("/{id}")
+	// identifica uma variavel nesse cado é do tipo Long que se chama id
+	public ResponseEntity<Postagem> getById(@PathVariable Long id) {
+		// ele vai no banco de dados e procura a postagem pela id, se encontrar ele
+		// armazena na resposta e retorna
+		// se não encontrar ele retorna uma mensagem de erra (404) e fecha a ação.
+		return postagemRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-		
-	}
-	
-	
-}
 
+	}
+
+	// retorno de select
+	// nome + variavel(nome do titulo)
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) {
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+
+	// INSERT
+//verbo post é usado para fazer o insert no DB
+	@PostMapping
+//post nome da variavel
+	// Valid = checa as validações na model
+	// requestBody =
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+	}
+
+	// UPDATE
+	@PutMapping
+	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
+
+		return postagemRepository.findById(postagem.getId())
+
+				// .map mapeia a postagem que foi localizado passando o Id//
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+
+	
+	
+	//DELETE
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		
+//TRATANDO E PREVININDO ERROS NO RETNORNO DA POSTAGEM QUE FOI PESQUISADA
+		Optional<Postagem> postagem = postagemRepository.findById(id);
+		if (postagem.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		
+		postagemRepository.deleteById(id);
+
+	}
+}
--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,6 +1,9 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.generation.blogpessoal.model.Postagem;
 
@@ -8,6 +11,13 @@ import com.generation.blogpessoal.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 
+//05/03
+//matodo especifico para buscar pelo titulo 
 	
+                              //esse titulo é da coluna do DB  //parametro (titulo)
+public List <Postagem> findAllByTituloContainingIgnoreCase    (@Param("titulo")String titulo);
 	
 }
+
+
+
